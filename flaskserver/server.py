4c6735ef38d838ff1ac81fffd61f395d398f09a4
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-14 16:38:25 +0530

    :package: Adding Prediction route to server file

--- flaskserver/server.py
+++ flaskserver/server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from controllers.api import HelloWorld
+from controllers.prediction import Prediction
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,6 +11,8 @@ api = Api(app)
 #     return "hello World"
 
 api.add_resource(HelloWorld, "/")
+api.add_resource(Prediction, "/predict")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
