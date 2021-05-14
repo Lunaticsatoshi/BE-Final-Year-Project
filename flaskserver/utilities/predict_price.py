d0080ddfa5d28f3da351735c3636939686b281f5
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-14 17:08:26 +0530

    :package: predict pricing utility function

--- flaskserver/utilities/predict_price.py
+++ flaskserver/utilities/predict_price.py
@@ -4,5 +4,5 @@ def predict_gcp_price():
         clf = pickle.load(File)
     File.close()
     inputfeatures = [-2.60671617e-01,  1.59104719e+00, -9.82589357e-02,  1.33424975e+00, 4.10833097e+00,  1.80812449e-01,  6.37374727e-01, -1.72337139e+00]
-    forecast_list = clf.predict(inputfeatures)
+    forecast_list = clf.predict([inputfeatures])
     return forecast_list
