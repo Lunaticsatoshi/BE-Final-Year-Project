2aabb60f1effeec0150be4237d0ea0e98ecf9f5d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-15 16:54:02 +0530

    :package: aws price prediction model integrated to flask server

--- flaskserver/utilities/predict_price.py
+++ flaskserver/utilities/predict_price.py
@@ -2,6 +2,22 @@ import pickle
 from datetime import date, timedelta
 
 
+def predict_aws_price(input_features):
+    new_forecast = {}
+    with open("../models/aws_model.pkl","rb") as File:
+        clf = pickle.load(File)
+    File.close()
+    inputfeatures = input_features
+    forecast_list = clf.predict(inputfeatures).tolist()
+    current_date = date.today()
+    for forecast in forecast_list:
+        days_after = current_date+timedelta(days=30)
+        current_date = days_after
+        new_date = days_after.isoformat() 
+        new_forecast[new_date] = forecast
+    return new_forecast
+
+
 def predict_gcp_price(input_features):
     new_forecast = {}
     with open("../models/gcp_model.pkl","rb") as File:
