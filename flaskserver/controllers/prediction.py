22b38f9879b409e0b16bad37a60cb2d54e8de329
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-15 16:07:06 +0530

    :zap: added the required prediction arguments

--- flaskserver/controllers/prediction.py
+++ flaskserver/controllers/prediction.py
@@ -4,7 +4,17 @@ from utilities.predict_price import predict_gcp_price
 
 
 prediction_post_args = reqparse.RequestParser()
-prediction_post_args.add_argument("name", type=str, help="Send the name of the Prediction")
+prediction_post_args.add_argument("CPU Cores", type=int, help="Number of CPU cores is required", required=True)
+prediction_post_args.add_argument("Memory", type=int, help="Amount of Memory is required", required=True)
+prediction_post_args.add_argument("Bandwidth", type=int, help="Bandwidth is required", required=True)
+prediction_post_args.add_argument("Instances", type=int, help="Number of Instances are required", required=True)
+prediction_post_args.add_argument("Hour 1", type=int, help="Hour Start Price is required", required=True)
+prediction_post_args.add_argument("Hour 2", type=int, help="Hour End Price is required", required=True)
+prediction_post_args.add_argument("Day Start", type=int, help="Day Start Price is required", required=True)
+prediction_post_args.add_argument("Day End", type=int, help="Day End Price is required", required=True)
+prediction_post_args.add_argument("Month Start", type=int, help="Month Start Price is required", required=True)
+prediction_post_args.add_argument("Month End", type=int, help="Month End Price is required", required=True)
+
 
 class Prediction(Resource):
     def get(self):
