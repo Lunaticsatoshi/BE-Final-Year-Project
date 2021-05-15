d954ffe60e0185116a9e38ba25d3696674261c46
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-15 16:55:11 +0530

    :package: azure price prediction model integrated to flask server

--- flaskserver/utilities/predict_price.py
+++ flaskserver/utilities/predict_price.py
@@ -32,3 +32,18 @@ def predict_gcp_price(input_features):
         new_date = days_after.isoformat() 
         new_forecast[new_date] = forecast
     return new_forecast
+
+def predict_azure_price(input_features):
+    new_forecast = {}
+    with open("../models/azure_model.pkl","rb") as File:
+        clf = pickle.load(File)
+    File.close()
+    inputfeatures = input_features
+    forecast_list = clf.predict(inputfeatures).tolist()
+    current_date = date.today()
+    for forecast in forecast_list:
+        days_after = current_date+timedelta(days=30)
+        current_date = days_after
+        new_date = days_after.isoformat() 
+        new_forecast[new_date] = forecast
+    return new_forecast
