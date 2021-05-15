67090b8d03fea73852fcd6fd84e4e71ba4843a2e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-15 16:39:00 +0530

    :package: Getting the input array for prediction model

--- flaskserver/controllers/prediction.py
+++ flaskserver/controllers/prediction.py
@@ -1,6 +1,8 @@
 from flask import jsonify, request
 from flask_restful import Resource, reqparse
 from utilities.predict_price import predict_gcp_price
+from resources.data import get_data
+from resources.calculate_percent import calculate_percent
 
 
 prediction_post_args = reqparse.RequestParser()
@@ -23,6 +25,11 @@ class Prediction(Resource):
 
     def post(self):
         args = prediction_post_args.parse_args()
+        HR_PCT = calculate_percent(args["Hour 1"],args["Hour 2"])
+        DY_PCT = calculate_percent(args["Day Start"],args["Day End"])
+        MN_PCT = calculate_percent(args["Month Start"],args["Month End"])
+        data_array = [args["CPU Cores"],args["Memory"],args["Bandwidth"],args["Instances"],HR_PCT,DY_PCT,MN_PCT,args["Month End"]]
+        input_array = get_data(data_array)
         prediction_result = predict_gcp_price()
-        result = jsonify({"AWS": args["Memory"]})
+        result = jsonify({"AWS": input_array})
         return result
