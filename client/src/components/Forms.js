09cc6e1258ae9aab200cd2c3bd71958641338a75
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-18 17:06:24 +0530

    :lipstick: Designed Forms for user Input

--- client/src/components/Forms.js
+++ client/src/components/Forms.js
@@ -1,24 +1,67 @@
-import { Label, Input, Select, Textarea, Button, Slider, Box } from "theme-ui";
+import { Label, Input, Select, Button, Slider, Box, Grid } from "theme-ui";
 
 export default function Forms() {
   return (
     <Box as="form" sx={styles.forms} onSubmit={(e) => e.preventDefault()}>
-      <Label htmlFor="username">Username</Label>
-      <Input name="username" id="username" mb={3} sx={styles.forms.input} />
-      <Label htmlFor="username">Username</Label>
-      <Input name="username" id="username" mb={3} sx={styles.forms.input} />
-      <Label htmlFor="username">Username</Label>
-      <Input name="username" id="username" mb={3} sx={styles.forms.input} />
-      <Label htmlFor="username">Username</Label>
-      <Input name="username" id="username" mb={3} sx={styles.forms.input} />
-      <Label htmlFor="sound">Sound</Label>
-      <Select name="sound" id="sound" mb={3}>
-        <option>Beep</option>
-        <option>Boop</option>
-        <option>Blip</option>
+      <Label>CPU Cores</Label>
+      <Select name="CPUCores" id="CPUCores" mb={3} sx={styles.forms.selectInput}>
+        <option>2</option>
+        <option>4</option>
+        <option>8</option>
       </Select>
-      <Label>Slider</Label>
-      <Slider mb={3} />
+      <Label>Memory</Label>
+      <Select name="Memory" id="Memory" mb={3} sx={styles.forms.selectInput}>
+        <option>2</option>
+        <option>4</option>
+        <option>8</option>
+        <option>16</option>
+      </Select>
+      <Label>Bandwidth</Label>
+      <Select name="Bandwidth" id="Bandwidth" mb={3} sx={styles.forms.selectInput}>
+        <option>16</option>
+        <option>32</option>
+        <option>64</option>
+        <option>128</option>
+        <option>256</option>
+        <option>512</option>
+      </Select>
+      <Label>Instances</Label>
+      <Select name="Instances" id="Instances" mb={3} sx={styles.forms.selectInput}>
+        <option>2</option>
+        <option>4</option>
+        <option>6</option>
+        <option>8</option>
+      </Select>
+      <Grid gap={2} columns={[2, "1fr 1fr"]}>
+        <Box>
+          <Label>Hour 1</Label>
+          <Input name="Hour1" id="Hour1" mb={3} sx={styles.forms.input} />
+        </Box>
+        <Box>
+          <Label>Hour 2</Label>
+          <Input name="Hour2" id="Hour2" mb={3} sx={styles.forms.input} />
+        </Box>
+      </Grid>
+      <Grid gap={2} columns={[2, "1fr 1fr"]}>
+        <Box>
+        <Label>Day Start</Label>
+          <Input name="DayStart" id="DayStart" mb={3} sx={styles.forms.input} />
+        </Box>
+        <Box>
+        <Label>Day End</Label>
+          <Input name="DayEnd" id="DayEnd" mb={3} sx={styles.forms.input} />
+        </Box>
+      </Grid>
+      <Grid gap={2} columns={[2, "1fr 1fr"]}>
+        <Box>
+        <Label>Month Start</Label>
+          <Input name="MonthStart" id="MonthStart" mb={3} sx={styles.forms.input} />
+        </Box>
+        <Box>
+        <Label>Month End</Label>
+          <Input name="MonthEnd" id="MonthEnd" mb={3} sx={styles.forms.input} />
+        </Box>
+      </Grid>
       <Button>Submit</Button>
     </Box>
   );
@@ -33,7 +76,7 @@ const styles = {
     width: [null, null, "150px", "400px"],
     input: {
       borderColor: "gray",
-      width: ["auto", "auto", "400px"],
+      width: ["auto", "50", "auto"],
       height: "50px",
       borderRadius: "25px",
       "&:focus": {
@@ -41,6 +84,11 @@ const styles = {
         boxShadow: (t) => `0 0 0 2px ${t.colors.primary}`,
         outline: "none",
       },
+    },
+    selectInput: {
+      backgroundColor: "#000000",
+      color: "#FFFFFF",
+      borderColor: "gray",
     },
   },
 };
