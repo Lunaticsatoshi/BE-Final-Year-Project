a098a398b783a54640d6899ab966aa27fc81b84b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-22 17:09:53 +0530

    :zap: Removing prediction section from the sections

--- client/src/pages/prediction.js
+++ client/src/pages/prediction.js
@@ -5,7 +5,6 @@ import theme from "theme";
 import SEO from "components/seo";
 import Layout from "components/layoutApp";
 import Forms from "sections/form";
-import Prediction from "sections/prediction";
 export default function PredictionPage() {
   return (
     <ThemeProvider theme={theme}>
