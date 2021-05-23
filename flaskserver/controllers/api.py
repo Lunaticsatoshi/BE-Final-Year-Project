4d950d45443298cd71e39a706a8cba36321e4b9a
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-23 16:59:41 +0530

    :package: Implemented price comparison end point

--- flaskserver/controllers/api.py
+++ flaskserver/controllers/api.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from flask_restful import Resource
 
-class HelloWorld(Resource):
+class PriceComparison(Resource):
     def get(self):
-        result = jsonify({"message":"Hello world"})
+        result = jsonify({"hourly":{"Digital Ocean": "$0.13393", "Linode": "$0.045", "Vultr": "$0.060"},"monthly":{"Digital Ocean": "$90.00", "Linode": "$60.00", "Vultr": "$40.00"}})
         return result
